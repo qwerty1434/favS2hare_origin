39d9673e4bb747589c1b32c5012f55376ffece1e
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-08-01 14:21:02 +0900

    feat: bottom navigation bar router link

--- FrontEnd/favshare/src/components/BottomNavigationBar.vue
+++ FrontEnd/favshare/src/components/BottomNavigationBar.vue
@@ -1,28 +1,36 @@
 <template>
   <v-bottom-navigation :value="value" color="indigo" grow absolute>
-    <v-btn>
-      <span>Home</span>
+    <router-link :to="{ name: 'home' }" class="button">
+      <v-btn>
+        <span>Home</span>
 
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </router-link>
 
-    <v-btn>
-      <span>Pops</span>
+    <router-link :to="{ name: 'pops' }" class="button">
+      <v-btn>
+        <span>Pops</span>
 
-      <v-icon>mdi-party-popper</v-icon>
-    </v-btn>
+        <v-icon>mdi-party-popper</v-icon>
+      </v-btn>
+    </router-link>
 
-    <v-btn>
-      <span>Search</span>
+    <router-link :to="{ name: 'search' }" class="button">
+      <v-btn>
+        <span>Search</span>
 
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </router-link>
 
-    <v-btn>
-      <span>Profile</span>
+    <router-link :to="{ name: 'feed' }" class="button">
+      <v-btn>
+        <span>Profile</span>
 
-      <v-icon>mdi-account</v-icon>
-    </v-btn>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </router-link>
   </v-bottom-navigation>
 </template>
 
@@ -37,4 +45,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.button {
+  padding-top: 8px;
+}
+</style>
