d4c47091ade087e2105346f3d3eaae7c75287875
조봉민 <nim924@naver.com>
2022-08-02 13:29:50 +0900

    [#4] add jwt / auth email

--- BackEnd/favshare/src/main/java/com/favshare/controller/UserController.java
+++ BackEnd/favshare/src/main/java/com/favshare/controller/UserController.java
@@ -5,11 +5,9 @@ import java.util.*;
 
 
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,34 +15,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.favshare.dto.UserAccountDto;
 import com.favshare.dto.UserSignUpDto;
-import com.favshare.repository.UserRepository;
 import com.favshare.service.UserService;
+import com.favshare.service.JwtServiceImpl;
 
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+	
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+	
+	@Autowired
+	private JwtServiceImpl jwtService;
+	
 	@Autowired
 	private UserService userService;
 	
 	@ApiOperation(value="로그인",response=ResponseEntity.class)
 	//get방식은 안됨 pathvariable만 가능한듯?
 	@PostMapping("/login")
-	public ResponseEntity userLogin(@RequestBody HashMap<String, String> userInfo) {
-        UserAccountDto result = userService.getByEmail(userInfo.get("email"));
+	public ResponseEntity<Map<String, Object>> userLogin(@RequestBody HashMap<String, String> userInfo) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			UserAccountDto result = userService.getByEmail(userInfo.get("email"));
+			if(userInfo.get("email").equals(result.getEmail())&&
+					userInfo.get("password").equals(result.getPassword())) {
+				String token = jwtService.create("userId", result.getId(), "access-token");// key, data, subject
+				resultMap.put("access-token", token);
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			}else {        	
+				resultMap.put("message", FAIL);
+				status = HttpStatus.ACCEPTED;
+			}
+			
+		}catch (Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
 
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
         // 보안 강화 필요
         // 반환값 필요한지 확인
-        if(userInfo.get("email").equals(result.getEmail())&&
-        		userInfo.get("password").equals(result.getPassword())) {
-        	System.out.println("succ");
-        	return new ResponseEntity(HttpStatus.OK);
-        }else {        	
-        	System.out.println("fail");
-        	return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
 
 	}
 	
--- BackEnd/favshare/src/main/java/com/favshare/controller/UserPasswordController.java
+++ BackEnd/favshare/src/main/java/com/favshare/controller/UserPasswordController.java
@@ -2,19 +2,21 @@ package com.favshare.controller;
 
 import java.util.HashMap;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.favshare.dto.UserAccountDto;
-import com.favshare.dto.UserSignUpDto;
 import com.favshare.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
@@ -23,6 +25,14 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/user/password")
 public class UserPasswordController {
 
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	private final int COUNT = 15;
+	
+	private static final String FROM_ADDRESS = "<favshare@ssafy.com>";
+	
+	private String random;
 	@Autowired
 	private UserService userService;
 
@@ -37,18 +47,48 @@ public class UserPasswordController {
 
 		}
 	}
-
-	@ApiOperation(value = "인증번호 입력 후 확인 클릭시", response = ResponseEntity.class)
-	@GetMapping("/auth/input")
-	public void checkAuth(@RequestBody HashMap<String, String> userInfo) {
-		// 인증번호  완료 후 작업 필요
-	}
-
+	
 	@ApiOperation(value = "인증번호 발송 클릭 시 ", response = ResponseEntity.class)
 	@GetMapping("/sendAuth/{email}")
 	public void sendAuth(@PathVariable("email") String email) {
-		// 이메일 전송
-
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper;
+		try {
+			helper = new MimeMessageHelper(message, true);
+			helper.setFrom("favshare"+FROM_ADDRESS);
+			helper.setTo(email);
+			helper.setSubject("favshare 인증번호 안내 메일입니다.");
+			String theAlphaNumericS;
+	        StringBuilder builder;
+	        
+	        theAlphaNumericS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	                                    + "0123456789"; 
+	        //create the StringBuffer
+	        builder = new StringBuilder(COUNT); 
+	        for (int m = 0; m < COUNT; m++) { 
+	            int myindex 
+	                = (int)(theAlphaNumericS.length() * Math.random()); 
+	            builder.append(theAlphaNumericS.charAt(myindex)); 
+	        } 
+	        random = builder.toString();
+			helper.setText("인증번호는 "+ random +" 입니다.", true);
+			mailSender.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		
 	}
+
+	@ApiOperation(value = "인증번호 입력 후 확인 클릭시", response = ResponseEntity.class)
+	@GetMapping("/auth/{input}")
+	public ResponseEntity<String> checkAuth(@PathVariable("input") String input) {
+		// 인증번호  완료 후 작업 필요
+		if(random != null && input.equals(random)) {
+			return new ResponseEntity("success",HttpStatus.OK);
+		}else {
+			return new ResponseEntity("fail",HttpStatus.OK);
+		}
+	}
+
 
 }
