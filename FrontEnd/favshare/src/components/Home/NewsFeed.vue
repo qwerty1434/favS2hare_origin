ee643935ecf369cde79aade8b503ad9b79e47475
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-08-08 12:02:55 +0900

    feat: homeview restapi수정, vuex수정

--- FrontEnd/favshare/src/components/Home/NewsFeed.vue
+++ FrontEnd/favshare/src/components/Home/NewsFeed.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- tmp html -->
   <div>
     <button @click="getLogin">[ 로그인하기 ]</button>
     <button @click="getLogout">[ 로그아웃하기 ]</button>
@@ -9,9 +10,19 @@
       <login-request-message></login-request-message>
     </div>
   </div>
+  <!-- for-use html -->
+  <!-- <div>
+    <div v-if="userId">
+      <news-feed-list></news-feed-list>
+    </div>
+    <div v-else>
+      <login-request-message></login-request-message>
+    </div>
+  </div> -->
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import LoginRequestMessage from "./LoginRequestMessage.vue";
 import NewsFeedList from "./NewsFeedList.vue";
 
@@ -23,7 +34,11 @@ export default {
       isLogin: true,
     };
   },
+  computed: {
+    ...mapGetters(["userId"]),
+  },
   methods: {
+    // 밑에 두개 다 tmp용
     getLogin() {
       this.isLogin = true;
     },
@@ -31,6 +46,12 @@ export default {
       this.isLogin = false;
     },
   },
+  // for-use watch
+  // watch: {
+  //   "$store.state.account.userId": function () {
+  //     console.log(this["$store.state.account.userId"]);
+  //   },
+  // },
 };
 </script>
 
