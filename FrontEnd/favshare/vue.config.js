ba6a3c936bd4a142dd9ead2ddc45d66e5835390c
이현빈 <amy7852@gmail.com>
2022-08-16 20:22:30 +0900

    fix: invalid host header

--- FrontEnd/favshare/vue.config.js
+++ FrontEnd/favshare/vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  //이렇게 추가?
   devServer: {
-    disableHostCheck: true,
+    allowedHosts: "all",
   },
 });
