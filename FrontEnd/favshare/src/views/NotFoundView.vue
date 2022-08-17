8295c1bc9dc69ea92d4f5468c27d08b494d1d7d6
이현빈 <amy7852@gmail.com>
2022-08-18 02:05:37 +0900

    style: pops related

--- FrontEnd/favshare/src/views/NotFoundView.vue
+++ FrontEnd/favshare/src/views/NotFoundView.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>404 Not Found</div>
+  <!-- eslint-disable -->
+  <v-container>
+    <div class="main-box">
+      <v-row class="err-font">
+        <v-col align="center">
+          <h1>404 Not Found</h1>
+        </v-col>
+      </v-row>
+      <v-row class="mt-n5 err-font-min">
+        <v-col align="center">
+          <h4>요청하신 페이지를 찾을 수 없습니다 ㅠㅠ</h4>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align="center">
+          <v-btn color="#ff5d5d" outlined elevation="5" @click="goHome">홈으로 이동</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "NotFoundView",
+  methods: {
+    goHome() {
+      this.$router.push({ name: "home" });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.err-font {
+  color: #ff5d5d;
+}
+.err-font-min {
+  color: #8a8484;
+}
+.main-box {
+  margin-top: 200px;
+}
+</style>
