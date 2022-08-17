90d7a6225b3cf5217ec77bbd31824ed78b5b38d2
조봉민 <nim924@naver.com>
2022-08-17 16:50:44 +0900

    fix: 정보 수정후 feed 화면으로 오기

--- FrontEnd/favshare/src/views/Profile/ProfileEditView.vue
+++ FrontEnd/favshare/src/views/Profile/ProfileEditView.vue
@@ -18,7 +18,11 @@
       </v-col>
       <v-col class="ml-n100">
         <div>
-          <v-file-input prepend-icon="mdi-image-plus-outline" hide-input label="프로필 사진 변경" />
+          <v-file-input
+            prepend-icon="mdi-image-plus-outline"
+            hide-input
+            label="프로필 사진 변경"
+          />
         </div>
       </v-col>
       <div></div>
@@ -27,18 +31,28 @@
       <v-row align="center">
         <v-col cols="3"><h5>닉네임</h5></v-col>
         <v-col class="ml-n4">
-          <v-text-field class="font-t" v-model="editUserInfo.nickname" dense> </v-text-field>
+          <v-text-field class="font-t" v-model="editUserInfo.nickname" dense>
+          </v-text-field>
         </v-col>
       </v-row>
       <v-row class="mt-n5 mb-5" align="center">
         <v-col cols="4"><h5>프로필 소개</h5></v-col>
         <v-col class="ml-n3">
-          <v-text-field class="font-t" v-model="editUserInfo.content" dense></v-text-field>
+          <v-text-field
+            class="font-t"
+            v-model="editUserInfo.content"
+            dense
+          ></v-text-field>
         </v-col>
       </v-row>
     </div>
     <div align="center" class="ma-10">
-      <v-btn class="white--text" color="grey darken-1" small rounded @click="updateProfile"
+      <v-btn
+        class="white--text"
+        color="grey darken-1"
+        small
+        rounded
+        @click="updateProfile"
         >수정완료</v-btn
       >
     </div>
@@ -78,7 +92,7 @@ export default {
         content: this.editUserInfo.content,
       });
       alert("프로필 정보가 수정되었습니다.");
-      this.$router.go();
+      this.$router.push({ name: "feed" });
     },
     goBack() {
       this.$router.go(-1);
